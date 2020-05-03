362c230b5d65d85860793513e5b97148237d2971
eduvizuete <vizuete.eduardo@gmail.com>
2020-05-03 10:15:01 -0500

    feature: league logger

--- backend/soccer-app/src/main/java/com/evizcloud/soccerapp/service/impl/LeagueServiceImpl.java
+++ backend/soccer-app/src/main/java/com/evizcloud/soccerapp/service/impl/LeagueServiceImpl.java
@@ -3,26 +3,32 @@ package com.evizcloud.soccerapp.service.impl;
 import com.evizcloud.soccerapp.persistence.model.League;
 import com.evizcloud.soccerapp.persistence.repository.ILeagueRepository;
 import com.evizcloud.soccerapp.service.ILeagueService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class ILeagueServiceImpl implements ILeagueService {
+public class LeagueServiceImpl implements ILeagueService {
 
-    private ILeagueRepository leagueRepository;
+    private static final Logger LOG = LoggerFactory.getLogger(LeagueServiceImpl.class);
 
-    public ILeagueServiceImpl(ILeagueRepository leagueRepository) {
+    private final ILeagueRepository leagueRepository;
+
+    public LeagueServiceImpl(ILeagueRepository leagueRepository) {
         this.leagueRepository = leagueRepository;
     }
 
     @Override
     public Optional<League> findById(Long id) {
+        LOG.info("League Service >> Finding League By Id {}", id);
         return leagueRepository.findById(id);
     }
 
     @Override
     public League save(League league) {
+        LOG.info("League Service >> Saving League {}", league);
         return leagueRepository.save(league);
     }
 }
